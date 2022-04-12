f7774d503a06544c3e1b5e2c6553fe8bc08b92b5
timonson <timo.rossa@tutanota.com>
2022-04-12 03:00:35 +0200

    Add Arial to button

--- static/src/deps.ts
+++ static/src/deps.ts
@@ -1,3 +1,3 @@
-export { NiceForm } from "https://deno.land/x/pouch@v0.0.2/mod.ts";
+export { NiceForm } from "https://deno.land/x/pouch@v0.0.3/mod.ts";
 
-export type { LabeledControls } from "https://deno.land/x/pouch@v0.0.2/mod.ts";
+export type { LabeledControls } from "https://deno.land/x/pouch@v0.0.3/mod.ts";
