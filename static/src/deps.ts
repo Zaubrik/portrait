b3a828e753f771fc4a6607439f1a51c6847d22cd
timonson <timo.rossa@tutanota.com>
2022-04-21 20:32:01 +0200

    Update

--- static/src/deps.ts
+++ static/src/deps.ts
@@ -1,3 +1,3 @@
-export { NiceForm } from "https://deno.land/x/pouch@v0.0.4/mod.ts";
+export { NiceForm } from "https://deno.land/x/pouch@v0.0.5/mod.ts";
 
-export type { LabeledControls } from "https://deno.land/x/pouch@v0.0.4/mod.ts";
+export type { LabeledControls } from "https://deno.land/x/pouch@v0.0.5/mod.ts";
