41eb14232f629eae0506de11392161c2f211305c
timonson <timo.rossa@tutanota.com>
2023-01-31 15:54:25 +0100

    -

--- deps.ts
+++ deps.ts
@@ -4,6 +4,6 @@ export {
   Image,
   type ImageFormat,
   SvgCanvas,
-} from "https://deno.land/x/skia_canvas@0.5.0/mod.ts";
+} from "https://deno.land/x/skia_canvas@0.5.1/mod.ts";
 
 export { parse } from "https://deno.land/x/std@0.173.0/path/mod.ts";
